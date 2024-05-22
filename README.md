998db3057b9eedc38fb7878e3bbf0b2c5eadc9cc
Kshi@3620 <kshitijnehete@gmail.com>
2024-05-22 22:27:00 +0530

    day 2 commit

--- README.md
+++ README.md
@@ -122,3 +122,6 @@ const myFunction = function(){
    console.log('Hello World');
 }
 ```
+
+
+
--- code.js
+++ code.js
@@ -1,11 +1,3 @@
-const heros = ["Spiderman", "Hanuman", "Batman", "Superman"]; // array
+// Stack (Primitive), Heap (Non-Primitive)
 
-let obj = {             // object
-    name : "Kshitij",
-    age : 22,
-}
-
-const myFunction = function(){
-   console.log('Hello World');
-}
-
+let myName = "Kshitij"
